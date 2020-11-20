27898f1a00dc6516e1568db7cddf9901d0de3f05
Mikhail Kondaurov <kma1571b@edu.ugrasu.ru>
2020-11-20 16:22:38 +0500

    added good model and commented admin

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+'''
+from .models import Postadmin.site.register(EnemyType)
+from .models import Postadmin.site.register(Enemy)
+from .models import Postadmin.site.register(Weapon)
 # Register your models here.
+admin.site.register(EnemyType)
+admin.site.register(Enemy)
+admin.site.register(Weapon)
+'''
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class EnemyType (models.Model):
+	type = models.CharField(max_length=50)
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.type
+
+class Enemy (models.Model):
+	name = models.CharField(max_length=50)
+	power = models.IntegerField()
+	shield = models.IntegerField()
+	etype = models.ForeignKey(EnemyType, on_delete=models.CASCADE, default = 0)
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.name
+
+class Weapon (models.Model):
+	title = models.CharField(max_length=50)
+	damage = models.IntegerField()
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.title
