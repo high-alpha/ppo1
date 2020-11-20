aebc22ed69f02a8152209deb1f424f9dc7c5bb0f
Mikhail Kondaurov <kma1571b@edu.ugrasu.ru>
2020-11-20 16:31:19 +0500

    items to db added, admin.py fixed

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 
-from .models import Postadmin.site.register(EnemyType)
-from .models import Postadmin.site.register(Enemy)
-from .models import Postadmin.site.register(Weapon)
+from .models import EnemyType
+from .models import Enemy
+from .models import Weapon
 # Register your models here.
 admin.site.register(EnemyType)
 admin.site.register(Enemy)
